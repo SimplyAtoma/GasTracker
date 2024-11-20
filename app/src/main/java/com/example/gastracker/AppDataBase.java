38aa0bed5f880c8e22921d198d5b066c31d3ff7f
DMartinez14 <david.martinez8312@gmail.com>
2024-11-20 15:04:51 -0800

    Did the login page page

---
+++ app/src/main/java/com/example/gastracker/AppDataBase.java
@@ -0,0 +1,5 @@
+package com.example.gastracker;
+
+public class AppDataBase {
+
+}
--- app/src/main/java/com/example/gastracker/MainActivity.java
+++ app/src/main/java/com/example/gastracker/MainActivity.java
@@ -8,17 +8,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.gastracker.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    int loggedInUserId = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
